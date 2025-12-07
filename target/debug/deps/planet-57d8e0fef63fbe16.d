9eb3395eaaf7b2e916b2251e969a55935ca0a18f
goran-devetak <goran.devetak@studenti.unitn.it>
2025-12-07 15:01:27 +0100

    stop tracking ignored files

--- target/debug/deps/planet-57d8e0fef63fbe16.d
+++
@@ -1,5 +0,0 @@
-/Users/gorandevetak/Desktop/University/AdvancedProgramming/progetto/TheCompilerStrikesBack/target/debug/deps/planet-57d8e0fef63fbe16.d: planet/src/lib.rs
-
-/Users/gorandevetak/Desktop/University/AdvancedProgramming/progetto/TheCompilerStrikesBack/target/debug/deps/libplanet-57d8e0fef63fbe16.rmeta: planet/src/lib.rs
-
-planet/src/lib.rs:
--- target/debug/deps/project-d0123d86d6b51bdf.d
+++
@@ -1,5 +0,0 @@
-/Users/gorandevetak/Desktop/University/AdvancedProgramming/progetto/TheCompilerStrikesBack/target/debug/deps/project-d0123d86d6b51bdf.d: project/src/main.rs
-
-/Users/gorandevetak/Desktop/University/AdvancedProgramming/progetto/TheCompilerStrikesBack/target/debug/deps/project-d0123d86d6b51bdf: project/src/main.rs
-
-project/src/main.rs:
